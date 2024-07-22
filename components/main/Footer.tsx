01240a8000b4e6f8d548814b6b6c35b0608ca281
giselle <giselle.mingue@gmail.com>
2024-07-21 22:04:22 -0400

    updating files

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -49,7 +49,7 @@ const Footer = () => {
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                     <div className="font-bold text-[16px]">About</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
+                        {/* <span className="text-[15px] ml-[6px]">Become Sponsor</span>     */}
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                       
@@ -57,7 +57,7 @@ const Footer = () => {
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
-                        <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>    
+                        <span className="text-[15px] ml-[6px]">Contact</span>    
                     </p>
                 </div>
             </div>
